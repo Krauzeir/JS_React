678736887338e8f6ffab85c5bc00e06da6d3d718
Soluevo Mac Mini 04 <mm04@Mac-mini-2.local>
2022-08-04 17:09:22 -0300

    Finalizado e corrigido

--- src/components/Colaborador/index.js
+++ src/components/Colaborador/index.js
@@ -1,17 +1,36 @@
 import './Colaborador.css'
 
-const Colaborador = () => {
+const Colaborador = (props) => {
     return (
-        <div>
-            <div>
-                <img src='' alt=''></img>
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src={props.imagem} alt={props.nome}></img>
             </div>
-            <div>
-                <h4>Bruno Sacramento</h4>
-                <h5>Frontend</h5>
+            <div className='rodape'>
+                <h4>{props.nome}</h4>
+                <h5>{props.cargo}</h5>
             </div>
         </div>
     )
 }
 
-export default Colaborador
+
+/* Outra forma de passar parâmetros é desestruturando-o*/
+
+
+const Colaborador2 = ({nome, cargo, imagem, corDeFundo}) => {
+    return (
+        <div className='colaborador'>
+            <div className='cabecalho' style={{backgroundColor: corDeFundo}}>
+                <img src={imagem} alt={nome}></img>
+            </div>
+            <div className='rodape'>
+                <h4>{nome}</h4>
+                <h5>{cargo}</h5>
+            </div>
+        </div>
+    )
+}
+
+
+export default Colaborador2
--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -6,11 +6,24 @@ const Time = (props) => {
     const css = { backgroundColor: props.corSecundaria }
 
     return (
-        <section className='time' style={css}>
+        (props.colaboradores.length > 0) && <section className='time' style={css}>
             <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
-            <Colaborador />
+            <div className='colaboradores'>
+                {props.colaboradores.map( colaborador => <Colaborador key={colaborador.nome} corDeFundo={props.corPrimaria} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem}/>)}
+            </div>
         </section>
     )
+    /* 
+    return (
+        (props.colaboradores.length > 0) ? <section className='time' style={css}>
+            <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+            <div className='colaboradores'>
+                {props.colaboradores.map( colaborador => <Colaborador key={colaborador.index} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem}/>)}
+            </div>
+        </section>
+        : ''
+    )
+    */
 }
 
 export default Time
